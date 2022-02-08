c7f313f36c2f2ecfad1d0f380a20d5ec1ca87589
Ahmet Talha Köylü <ahmettalha.net@gmail.com>
2022-02-08 17:02:07 +0300

    Initial Commit

---
+++ DegreeMobile/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, Text } from "react-native"
+
+const App = () => {
+  return(
+    <SafeAreaView>
+      <Text>
+        Deneme
+      </Text>
+    </SafeAreaView>
+  )
+}
+
+export default App;
