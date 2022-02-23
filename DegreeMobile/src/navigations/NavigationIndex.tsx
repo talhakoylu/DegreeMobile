251bf25fab3588fa80da44419634af0b259c8b51
Ahmet Talha Köylü <ahmettalha.net@gmail.com>
2022-02-23 12:09:55 +0300

    Navigation Import Refactor

--- DegreeMobile/src/navigations/NavigationIndex.tsx
+++ DegreeMobile/src/navigations/NavigationIndex.tsx
@@ -1,33 +1,27 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import ChangePasswordScreen from '@views/screens/dashboard/ChangePasswordScreen';
-import DashboardHomeScreen from '@views/screens/dashboard/DashboardHomeScreen';
-import DashboardSettingsScreen from '@views/screens/dashboard/DashboardSettingsScreen';
-import LastSolvedQuizesScreen from '@views/screens/dashboard/LastSolvedQuizesScreen';
-import UserSettingsScreen from '@views/screens/dashboard/UserSettingsScreen';
-import HomeScreen from '@views/screens/HomeScreen';
-import QuizDetailQuestionsAndAnswersScreen from '@views/screens/QuizDetailQuestionsAndAnswersScreen';
-import QuizPostDetailScreen from '@views/screens/QuizPostDetailScreen';
+import { ChangePasswordScreen, DashboardHomeScreen, DashboardSettingsScreen, LastSolvedQuizesScreen, UserSettingsScreen, HomeScreen, QuizDetailQuestionsAndAnswersScreen, QuizPostDetailScreen } from '@views/screens/Index';
+
 import React from 'react'
 import { navigationRef } from './NavigationRef';
 
 const Stack = createNativeStackNavigator();
 
 const NavigationIndex: React.FC = () => {
-    
+
     return (
         <NavigationContainer ref={navigationRef}>
-            <Stack.Navigator  screenOptions={{
+            <Stack.Navigator screenOptions={{
                 headerShown: false
             }}>
-                <Stack.Screen name="Home" component={HomeScreen}/>
+                <Stack.Screen name="Home" component={DashboardHomeScreen} />
                 <Stack.Screen name="DashboardHome" component={DashboardHomeScreen} />
                 <Stack.Screen name="DashboardSettings" component={DashboardSettingsScreen} />
                 <Stack.Screen name="UserSettings" component={UserSettingsScreen} />
                 <Stack.Screen name="ChangePassword" component={ChangePasswordScreen} />
                 <Stack.Screen name="LastSolvedQuizes" component={LastSolvedQuizesScreen} />
                 <Stack.Screen name="QuizPostDetail" component={QuizPostDetailScreen} />
-                <Stack.Screen name="QuizPostQuestionsAnswers"  options={{headerShown: true, title: "Question & Answers"}} component={QuizDetailQuestionsAndAnswersScreen} />
+                <Stack.Screen name="QuizPostQuestionsAnswers" options={{ headerShown: true, title: "Question & Answers" }} component={QuizDetailQuestionsAndAnswersScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     )
---
+++ DegreeMobile/src/views/screens/Index.tsx
@@ -0,0 +1,10 @@
+import ChangePasswordScreen from "./dashboard/ChangePasswordScreen";
+import DashboardHomeScreen from "./dashboard/DashboardHomeScreen";
+import DashboardSettingsScreen from "./dashboard/DashboardSettingsScreen";
+import LastSolvedQuizesScreen from "./dashboard/LastSolvedQuizesScreen";
+import UserSettingsScreen from "./dashboard/UserSettingsScreen";
+import HomeScreen from "./HomeScreen";
+import QuizDetailQuestionsAndAnswersScreen from "./QuizDetailQuestionsAndAnswersScreen";
+import QuizPostDetailScreen from "./QuizPostDetailScreen";
+
+export {ChangePasswordScreen, DashboardHomeScreen, DashboardSettingsScreen, LastSolvedQuizesScreen, UserSettingsScreen, HomeScreen,QuizDetailQuestionsAndAnswersScreen, QuizPostDetailScreen}
